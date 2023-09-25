466eccfe14962f2b092c75080ee708080812ba42
Ayush Singh <ayushsinghmi711@gmail.com>
2023-09-25 09:46:53 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{html,ts,tsx,jsx,js}", "./src/*.{ts,tsx,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
